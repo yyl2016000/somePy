b36fa20289c67242e0ce3e4f572d4114c1e83c22
Lin <yyl2016000@outlook.com>
2019-06-17 20:37:22 +0800

    题解

--- leetcode_067.py
+++ leetcode_067.py
@@ -1,2 +1,42 @@
-a_str = "1001"
-print(len(a_str))
+def main(a :str, b: str) -> str:
+    la,lb = len(a), len(b)
+    ta,tb = a,b
+    if la < lb:
+        ta,tb = tb,ta
+        la,lb = lb,la
+    bu = la-lb
+    up = '0'
+    out_str=[]
+    for i in range(lb-1,-1,-1):
+        boolean = ta[i+bu] != tb[i]
+        if boolean:
+            if up == '1':
+                out_str.append('0')
+                up = '1'
+            else:
+                out_str.append('1')
+                up = '0'
+        else:
+            out_str.append(up)
+            if ta[i+bu] and tb[i] == '1':
+                up = '1'
+            else:
+                up = '0'
+    for i in range(la-lb-1,-1,-1):
+        if up == '0':
+            out_str.append(ta[i])
+        else:
+            if ta[i] == '0':
+                out_str.append('1')
+                up = '0'
+            else:
+                out_str.append('0')
+                up = '1'
+    if up == '1':
+        out_str.append('1')
+    out_str.reverse()
+    return "".join(out_str)
+if __name__ == '__main__':
+    a = input('输入a的值')
+    b = input('输入b的值')
+    print(main(a, b))
